2a3e2903c90083e5987eca6156f8bd2dec18a751
pySin <sinan_km@yahoo.com>
2025-03-03 17:20:21 +0000

    Regression set up

--- Regression/regression.py
+++ Regression/regression.py
@@ -8,16 +8,26 @@ class Regression:
         self.x_values_iv = None
         self.y_values_dv = None
 
-    @staticmethod
-    def receive_data(x_values_iv, y_values_dv):
+    def receive_data(self, x_values_iv, y_values_dv):
         if len(x_values_iv) != len(y_values_dv):
             raise ValueError("The amount of x values has to be the same as the amount of y values!")
 
         if not all(map(isdigit, x_values_iv)):
             raise ValueError("All x values has to be numeric!")
 
+        if not all(map(isdigit, y_values_dv)):
+            raise ValueError("All y values has to be numeric!")
 
-    def slope_calculate(self):
+        if type(x_values_iv) != list or type(y_values_dv) != list:
+            raise TypeError("All independent and dependent values(x, y) must be in a list!")
+
+        self.x_values_iv = x_values_iv
+        self.y_values_dv = y_values_dv
+
+
+    def slope_calculate(self, x_values_iv, y_values_dv):
+        self.receive_data(x_values_iv, y_values_dv)
+
         x_mean = sum(self.x_values_iv) / len(self.x_values_iv)
         y_mean = sum(self.y_values_dv) / len(self.y_values_dv)
         print(f"X mean: {x_mean}")
