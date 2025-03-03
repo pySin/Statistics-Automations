efb46c28aef3b04f472628a4ddd88e7506433f4c
pySin <sinan_km@yahoo.com>
2025-03-03 16:31:52 +0000

    Regression set up

---
+++ Regression/main.py
@@ -0,0 +1,10 @@
+from regression import Regression
+
+
+x_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+y_values = [25, 38, 29, 115, 82, 114, 120, 160, 153, 190, 239, 175]
+
+
+if __name__ == "__main__":
+    Regression.receive_data(x_values, y_values)
+
--- Regression/regression.py
+++ Regression/regression.py
@@ -1,3 +1,14 @@
 # Regression
 
 
+class Regression:
+
+    def __init__(self):
+        name = None
+
+    def receive_data(self, x_values_iv, y_values_dv):
+        x_mean = sum(x_values_iv) / len(x_values_iv)
+        y_mean = sum(y_values_dv) / len(y_values_dv)
+        print(f"X mean: {x_mean}")
+        print(f"Y mean: {y_mean}")
+
