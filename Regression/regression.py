46775513c641dad3bee2323b428a68d381455f65
pySin <sinan_km@yahoo.com>
2025-03-03 16:44:01 +0000

    Regression set up

--- Regression/regression.py
+++ Regression/regression.py
@@ -6,9 +6,19 @@ class Regression:
     def __init__(self):
         name = None
 
-    def receive_data(self, x_values_iv, y_values_dv):
+    @staticmethod
+    def receive_data(x_values_iv, y_values_dv):
         x_mean = sum(x_values_iv) / len(x_values_iv)
         y_mean = sum(y_values_dv) / len(y_values_dv)
         print(f"X mean: {x_mean}")
         print(f"Y mean: {y_mean}")
+
+        xy_summation = 0
+        x_2_summation = 0
+        for i in range(len(x_values_iv)):
+            xy_summation += (x_values_iv[i] - x_mean) * (y_values_dv - y_mean)
+            x_2_summation += (x_values_iv[i] - x_mean) ** 2
+
+        slope = xy_summation / x_2_summation
+        print(f"Slope: {slope}")
 
