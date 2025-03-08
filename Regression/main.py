e43b807e05608550524e69c48e136e74c68dbd09
pySin <sinan_km@yahoo.com>
2025-03-08 17:27:38 +0000

    Sort function call

--- Regression/main.py
+++ Regression/main.py
@@ -6,7 +6,12 @@ y_values = [25, 38, 29, 115, 82, 114, 120, 160, 153, 190, 239, 175]
 
 
 if __name__ == "__main__":
+    database = "world"
+    table = "country"
+    numeric_columns = ["LifeExpectancy", "GNP", "GNPOld"]
 
+    process_data = ProcessData(database, table, numeric_columns)
+    column_combinations = process_data.columns_combinations()
 
     regression_c = Regression()
     regression_c.slope_calculate(x_values, y_values)
